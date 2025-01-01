00c842a3cd5d1d34addae945134c3b4fb574f4f5
NI Rail Buddy <143949438+NI-Rail-Buddy@users.noreply.github.com>
2025-01-01 01:47:07 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+## YOUTUBE VIDEO: https://www.youtube.com/watch?v=dam0GPOAvVI
+## I am 41:05 minutes in.
+
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
